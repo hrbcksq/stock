9bc1fed297a50ddc28f9c821c8157d83e292b576
hrbkcsq <hrbcksq@gmail.com>
2017-10-30 01:37:08 +0200

    uncomment

--- src/app/data/provider/abstract/socket.service.ts
+++ src/app/data/provider/abstract/socket.service.ts
@@ -11,7 +11,7 @@ export abstract class SocketService {
 
   constructor(url: string) {
     if (!SocketService.socketInstance) {
-      // SocketService.socketInstance = new WebSocket(url);
+      SocketService.socketInstance = new WebSocket(url);
     };
   }
 
